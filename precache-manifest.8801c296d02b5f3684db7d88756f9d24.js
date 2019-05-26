f42c8f4d3088214ad5e7304b8b8dac5018019d1c
User <den.dmytruk@gmail.com>
2019-05-26 20:12:05 +0300

    Updates

---
+++ precache-manifest.8801c296d02b5f3684db7d88756f9d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3813a4495048e0738404fedf43b33d7b",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "8246d3d66d94bbe3696b",
+    "url": "/todo-react/static/css/main.a203444a.chunk.css"
+  },
+  {
+    "revision": "c0083dd73685f7f5152d",
+    "url": "/todo-react/static/js/2.be6699a7.chunk.js"
+  },
+  {
+    "revision": "8246d3d66d94bbe3696b",
+    "url": "/todo-react/static/js/main.fed10cea.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/todo-react/static/media/react-icon.05a3e30f.svg"
+  }
+]);
